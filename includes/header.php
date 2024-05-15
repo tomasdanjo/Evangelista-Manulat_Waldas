c781c9c2538b95698fca0054b4e723214df7159d
tomasdanjo <danthorium25@gmail.com>
2024-05-15 18:04:18 +0800

    added some functions for account verification

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -18,7 +18,6 @@ session_start();
 include("connect.php");
 include("login.php");
 include("register.php");
-include_once("functions.php");
 ?>
 
 <div class="nav">
@@ -50,24 +49,27 @@ include_once("functions.php");
     </li>';
     }
 
-    if (isset($_SESSION['username']) && ($_SESSION['username']) == "admin admin") {
+
+    if (isset($_SESSION['username']) && ($_SESSION['username']) == "admin") {
 
         echo '
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
-            Menu
-        </a>
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="account.php">Account</a></li>
-            <li><a class="dropdown-item" href="user.php">User</a></li>
-            <li><a class="dropdown-item" href="wallet.php">Wallets</a></li>
-            <li><a class="dropdown-item" href="transactions.php">Transactions</a></li>
-            <li><a class="dropdown-item" href="reports.php">Reports</a></li>
-            <li><a class="dropdown-item" href="stats.php">Statistics</a></li>
-            </ul>
-        </li>';
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">
+                        Menu
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="account.php">Account</a></li>
+                        <li><a class="dropdown-item" href="user.php">User</a></li>
+                        <li><a class="dropdown-item" href="wallet.php">Wallets</a></li>
+                        <li><a class="dropdown-item" href="transactions.php">Transactions</a></li>
+                        <li><a class="dropdown-item" href="reports.php">Reports</a></li>
+                        </ul>
+                    </li>';
     }
     ?>
+
+
+
 </div>
 
 <script src="js/script.js"></script>
